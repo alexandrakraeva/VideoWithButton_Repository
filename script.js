5c6f97e5e2fd91b8c7145f8b7b06addebcbe8889
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2023-11-26 17:23:15 +0100

    added files

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.getElementById('Cartoon');
+    const playButton = document.getElementById('playButton');
+    const playButtonContainer = document.getElementById('playButtonContainer');
+
+    playButton.addEventListener('click', () => {
+        video.style.display = 'block'; // Show the video
+        playButtonContainer.style.display = 'none'; // Hide the play button
+        video.play();
+    });
+
+    video.addEventListener('loadedmetadata', () => {
+        // Optionally, do something when video metadata is loaded
+    });
+
+    video.onended = function () {
+        // Optional: Redirect or perform an action after the video ends
+    };
+});
